d8c690f2afad64bdffa39a1e55853e19e4a30d11
ghgemmer <ghgemmer@gmail.com>
2021-02-15 14:57:34 -0600

    Corrected potential PROGMEM const variables access issue
    Most PROGMEM  const variables needs to be in lower 64K  of flash as accessing most of them assumes this.  So added a PROGMEMSECTION2 to move those that are always accessed as far addresses (can be anywhere in the address space) into that new region to avoid having PROGMEM  const variables overflow beyond lower 64K regsion So moved the Dayo, and WereWolves of London setups into PROGMEMSECTION2 areas are assumed to be anywhere in flash memory and accessed via far routines.

--- Sandbox/program_memory_misc.h
+++ Sandbox/program_memory_misc.h
@@ -4,6 +4,15 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 
+// This macro when applied to a constant variable places them in flash memory after any PROGMEM section in memory (after but 
+// not necessarily immediately after that section).
+// This is accomplished by placing the PROGMEMSECTION2 after the .text section in the linker which is definitely after the PROGMEM section.
+// This facilitates keeping constant variables with the PROGMEM attribute applied to them in the lower 64K of flash memory
+// where they generally need to be as most of these are accessed with pgmspace routines that assume this.
+// !!!! Access of a PROGMEMSECTION2 constant variable requires use of the ...far routines from pgmspace.h
+#define PROGMEMSECTION2 __attribute__ ((section (".text.higher"))) 
+// The following is now defined in pgmspace.h so is compiled out here.  Remove once verify code works ok
+#if 0
 #define pgm_get_far_address(var) \
 ({                                                    \
 	uint_farptr_t tmp;                                \
@@ -21,5 +30,5 @@
     );                                                \
     tmp;                                              \
 })
-
+#endif
 #endif
