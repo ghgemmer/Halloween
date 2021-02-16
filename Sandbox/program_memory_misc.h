f2a9822f40a31aee1c10cf64b9ef30dcda26e8fd
ghgemmer <ghgemmer@gmail.com>
2021-02-16 16:24:29 -0600

    Gained back more RAM space putting last prop values in PROGMEMSECTION2 memory area

--- Sandbox/program_memory_misc.h
+++ Sandbox/program_memory_misc.h
@@ -11,24 +11,5 @@
 // where they generally need to be as most of these are accessed with pgmspace routines that assume this.
 // !!!! Access of a PROGMEMSECTION2 constant variable requires use of the ...far routines from pgmspace.h
 #define PROGMEMSECTION2 __attribute__ ((section (".text.higher"))) 
-// The following is now defined in pgmspace.h so is compiled out here.  Remove once verify code works ok
-#if 0
-#define pgm_get_far_address(var) \
-({                                                    \
-	uint_farptr_t tmp;                                \
-                                                      \
-    __asm__ __volatile__(                             \
-                                                      \
-            "ldi    %A0, lo8(%1)"           "\n\t"    \
-            "ldi    %B0, hi8(%1)"           "\n\t"    \
-            "ldi    %C0, hh8(%1)"           "\n\t"    \
-            "clr    %D0"                    "\n\t"    \
-        :                                             \
-            "=d" (tmp)                                \
-        :                                             \
-            "p"  (&(var))                             \
-    );                                                \
-    tmp;                                              \
-})
-#endif
+
 #endif
